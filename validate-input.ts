a33107919291b6a7be8452f34b157de9298aaba6
elancuba <lancuba.emiliano@gmail.com>
2022-08-28 16:36:19 -0300

    fix validateInput func in utils/validate-input.ts

--- validate-input.ts
+++ validate-input.ts
@@ -3,10 +3,11 @@ import {validationResult} from "express-validator";
 import {CustomError} from "./http-response";
 import httpStatus from "http-status-codes";
 
-export function validateInput(req : Request){
-    if (!validationResult(req).isEmpty()){
+export function validateInput(req: Request) {
+    if (!validationResult(req).isEmpty()) {
         throw new CustomError(httpStatus.INTERNAL_SERVER_ERROR,
             "Error de validación de tipos para "
-            + validationResult(req).array().map((x) => x.param).join(', ')
-            + ". Asegurese de que haya ingresado los valores correctamente");    }
+            + validationResult(req).array().map((x: { param: any; }) => x.param).join(', ')
+            + ". Asegúrese de que haya ingresado los valores correctamente");
+    }
 }
