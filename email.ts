cc8d37be64745bf609fc6e5820921813ba2d3a64
Maximiliano Rodrigo Di Lorenzo <maximiliano.rdl@gmail.com>
2022-09-20 23:55:35 -0300

    Limpieza

--- email.ts
+++ email.ts
@@ -1,38 +1,26 @@
-import { Client } from "@sendgrid/client";
-import SendGrid from '@sendgrid/mail';
 import nodemailer from 'nodemailer';
 import SMTPTransport from "nodemailer/lib/smtp-transport";
-SendGrid.setClient(new Client());
-SendGrid.setApiKey(process.env.SENDGRID_API_KEY || "")
 
-// create reusable transporter object using the default SMTP transport
+// Create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
     host: process.env.SPARKPOST_HOST,
     port: process.env.SPARKPOST_PORT || 0,
     secure: false, // true for 465, false for other ports
     auth: {
-        user: process.env.SPARKPOST_USERNAME || "", // generated ethereal user
-        pass: process.env.SPARKPOST_PASSWORD || "", // generated ethereal password
+        user: process.env.SPARKPOST_USERNAME || "",
+        pass: process.env.SPARKPOST_PASSWORD || "",
     },
 } as SMTPTransport.Options);
 
 export class EmailManager {
     async send(input: Email): Promise<boolean> {
-        /*
-        return SendGrid.send(input).then(_ => {
-            return true
-        }).catch((err) => {
-            console.log(err)
-            throw err
-        })
-        */
         // send mail with defined transport object
         return await transporter.sendMail({
             from: `"Hospital Favaloro" <${input.from}>`, // sender address
-            to: input.to, // list of receivers
-            subject: input.subject, // Subject line
-            text: input.text, // plain text body
-            html: input.html, // html body
+            to: input.to,           // list of receivers
+            subject: input.subject,
+            text: input.text,
+            html: input.html,
         }).then(_ => {
             return true;
         }).catch((err) => {
