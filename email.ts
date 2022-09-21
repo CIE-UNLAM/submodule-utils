0432231f2c926245d46c00b7860afbabef3e5a77
Maximiliano Rodrigo Di Lorenzo <maximiliano.rdl@gmail.com>
2022-09-20 23:29:55 -0300

    added sparkpost transporter to email.ts

--- email.ts
+++ email.ts
@@ -1,16 +1,43 @@
 import { Client } from "@sendgrid/client";
 import SendGrid from '@sendgrid/mail';
+import nodemailer from 'nodemailer';
 SendGrid.setClient(new Client());
 SendGrid.setApiKey(process.env.SENDGRID_API_KEY || "")
 
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    host: process.env.SPARKPOST_HOST,
+    port: process.env.SPARKPOST_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: process.env.SPARKPOST_USERNAME, // generated ethereal user
+        pass: process.env.SPARKPOST_PASSWORD, // generated ethereal password
+    },
+});
+
 export class EmailManager {
-    send(input: Email): Promise<boolean> {
+    async send(input: Email): Promise<boolean> {
+        /*
         return SendGrid.send(input).then(_ => {
             return true
         }).catch((err) => {
             console.log(err)
             throw err
         })
+        */
+        // send mail with defined transport object
+        return await transporter.sendMail({
+            from: `"Hospital Favaloro" <${input.from}>`, // sender address
+            to: input.to, // list of receivers
+            subject: input.subject, // Subject line
+            text: input.text, // plain text body
+            html: input.html, // html body
+        }).then(_ => {
+            return true;
+        }).catch((err) => {
+            console.log(err)
+            throw err
+        });
     }
 }
 
